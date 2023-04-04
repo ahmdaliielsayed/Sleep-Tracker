de34abe6d5f592df4aebeaa15f50b51bb9e38897
SharafElDinA <ahmed.sharafeldin@vodafone.com>
2023-04-04 05:51:05 +0200

    - Replacing TextView with RecyclerView - Using DataBinding with RecyclerView - Using RecyclerView.Adapter then converted into ListAdapter with DiffUtil

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -67,6 +67,19 @@ class SleepTrackerFragment : Fragment() {
         binding.sleepTrackerViewModel = sleepTrackerViewModel
         binding.lifecycleOwner = this
 
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter = adapter
+
+        sleepTrackerViewModel.nights.observe(
+            viewLifecycleOwner,
+            Observer {
+                it?.let {
+//                    adapter.data = it
+                    adapter.submitList(it)
+                }
+            }
+        )
+
         sleepTrackerViewModel.navigateToSleepQuality.observe(
             viewLifecycleOwner,
             Observer { night ->
